88fb012ec358c484bfec06064fb1abc247ef51e3
Miraziz Akmalov <mirazizakmalov@gmail.com>
2024-06-05 11:20:32 -0400

    Candlestick Graph Option Added

--- Stock Chart.py
+++ Stock Chart.py
@@ -1,14 +1,16 @@
 import pandas as pd 
 import yfinance as yf
 import datetime
-import plotly.express as px
-import plotly.graph_objects as go
 from datetime import date, timedelta
 
-today = date.today()
+import plotly.express as px
+import plotly.graph_objects as go
 
+
+#Variables
 ticker = input("What stock ticker would you like to look at: " )
-
+choice = input("What would you like to see?")
+today = date.today()
 d1 = today.strftime("%Y-%m-%d")
 end_date = d1
 d2 = date.today() - timedelta(days=360)
@@ -16,11 +18,29 @@ d2 = d2.strftime("%Y-%m-%d")
 start_date = d2
 data = yf.download(ticker, start = start_date, end = end_date, progress=False)
 
-figure = px.line(data, x = data.index, y = "Close")
-figure.update_layout(title = ticker + " Stock Price Analysis",  xaxis_rangeslider_visible=True)
-print(data.head())
-#figure.show()
+print(data)
+ 
+# #Table
+# print(data.head())
 
-#data = pd.read_csv("AAPL.csv")
-#figure = go.Figure(go.Candlestick)
+# #Line Graph
+# figure = px.line(data, x = data.index, y = "Close")
+# figure.update_layout(title = ticker + " Stock Price Analysis",  xaxis_rangeslider_visible=True)
+# figure.show()
+
+#CandleStick Graph
+figure = go.Figure(data=[go.Candlestick(
+    x=data.index,
+    open=data["Open"],
+    high=data["High"],
+    low=data["Low"],
+    close=data["Close"]
+)])
+
+figure.update_layout(
+    title=f"{ticker} Stock Price Analysis",
+    xaxis_rangeslider_visible=False
+)
+
 figure.show()
+
